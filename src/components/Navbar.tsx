463afb236c66408e1b24eb38bab7be8dbc41a53d
Nmesoma Udojike <udojyk@gmail.com>
2023-11-28 10:22:42 +0100

    show only store in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,12 @@ export function Navbar() {
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to="/" as={NavLink}>
+{/*           <Nav.Link to="/" as={NavLink}>
             Home
           </Nav.Link>
           <Nav.Link to="/about" as={NavLink}>
             About
-          </Nav.Link>
+          </Nav.Link> */}
           <Nav.Link to="/store" as={NavLink}>
             Store
           </Nav.Link>
